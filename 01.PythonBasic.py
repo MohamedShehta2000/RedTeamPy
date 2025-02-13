f7fe38a0ff1353fd74f4213567ddfcb9b5d10ad1
Mo.shehta <mohamedshehta2024@gmail.com>
2025-02-14 00:46:14 +0200

    rename

---
+++ 01.PythonBasic.py
@@ -0,0 +1,15 @@
+# TO open file in linux
+#!usr/bin/python3
+file=open("kali/file.txt","r")
+print(open.r())
+
+# To Write file in Linux
+#!usr/bin/python3
+file=open("home/kali/file.txt","w")
+file.write("this is new line \n")
+file.close()
+
+#To excute linux commands with python
+#!usr/bin/python3
+import os
+os.system("ls -lah")
