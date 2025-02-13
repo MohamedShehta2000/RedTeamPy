f268446e148a75123f9638dbd3bc2741e9da241a
Mo.shehta <mohamedshehta2024@gmail.com>
2025-02-14 00:13:25 +0200

    python_OOP_

---
+++ 9-Python_OOP_2.py
@@ -0,0 +1,10 @@
+class newclass:
+    def __init__(self, x):
+        print(x)
+object1 = newclass(1234)   
+
+class newclass:
+    def __init__(self, x=0):
+        print(x)
+object1 = newclass()
+
