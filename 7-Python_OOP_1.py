9703c5ea9a37008f170d5c297397778625abda49
Mo.shehta <mohamedshehta2024@gmail.com>
2025-02-13 22:25:27 +0200

    python_OOP1_2025

--- 7-Python_OOP_1.py
+++ 7-Python_OOP_1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+# الدالة الأولى تأخذ قيمتين ثابتتين وتعيد مجموعهما
 wordlist = ['hello', 'world', 'python', 'is', 'awesome', '123']
 
 # طباعة جميع الكلمات
@@ -10,3 +10,25 @@ for word in wordlist:
 for word in wordlist:
     if word == '123':  # استخدم المطابقة المباشرة بدلاً من "in"
         print('123')
+
+
+def sum():
+    x = 3
+    y = 4
+    return x + y
+new = sum()
+print(new)
+
+# الدالة الثانية تأخذ قيمة واحدة وتعيد مجموعها مع قيمة ثابتة
+def sum(x):
+    y = 4
+    return x + y
+new = sum(100)
+print(new)
+
+# الدالة الثالثة تأخذ قيمتين وتعيد جملة تحتوي على القيمتين ومجموعهما
+def sum(x, y):
+    word = f"the sum of {x} and {y} is {x + y}"
+    return word
+new = sum(100, 200)
+print(new)
